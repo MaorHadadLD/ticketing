10950492bfb1573550cd4bc72fd7d5067e8a0932
MaorHadadLD <maorhadad94@gmail.com>
2024-12-09 22:39:03 +0200

    Add ingress

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,7 +4,9 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
-
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi there!');
+});
 
 app.listen(300, () => {
     console.log('Listening on port 3000');
