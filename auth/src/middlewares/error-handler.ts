2550fd79ec07c3919283b9ef06f08b3fae39276e
MaorHadadLD <maorhadad94@gmail.com>
2024-12-20 23:40:16 +0200

    update error handler

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,8 +1,21 @@
 import {Request, Response, NextFunction} from 'express';
+import {RequestValidationError} from '../errors/request-validation-error';
+import {DatabaseConnectionError} from '../errors/database-validation-error';
 
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
-    
+    if (err instanceof RequestValidationError) {
+        const formattedErrors = err.errors.map((error) => {
+          if (error.type === 'field') {
+            return { message: error.msg, field: error.path };
+          }
+        });
+        return res.status(400).send({ errors: formattedErrors });
+      }
+
+    if (err instanceof DatabaseConnectionError) {
+        return res.status(500).send({errors: [{message: err.reason}]});
+    }
     res.status(400).send({
-        message: 'Something went wrong'
+        errors: [{message: 'Somthing went wrong'}]
     });
 };
