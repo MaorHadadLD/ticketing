a3ada1e720eaef31f2836e6a495c4115ba06d2b8
MaorHadadLD <maorhadad94@gmail.com>
2024-12-21 22:19:52 +0200

    updating error validation + connect to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,14 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
 import { json } from 'body-parser';
+import mongoose from 'mongoose';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
+import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json());
@@ -15,8 +18,30 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.use(errorHandler);
+app.all('*', async (req, res) => {
+    throw new NotFoundError();
+  });
+  
+  // Error handling middleware must be used last
+  app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+    errorHandler(err, req, res, next);
+  });
 
-app.listen(3000, () => {
-    console.log('Listening on port 3000');
-});
+  const start = async () => {
+        try {
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+        console.log('Connected to MongoDB');
+        } catch (err) {
+            console.log(err);
+        }
+
+        app.listen(3000, () => {
+            console.log('Listening on port 3000!!!!!!!!');
+         });
+    };
+
+    start();
+ 
+
+
+
