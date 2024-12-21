fb59417e1deb564311fc771c914fdb9e835b33bb
MaorHadadLD <maorhadad94@gmail.com>
2024-12-21 15:55:32 +0200

    update serializeErrors func

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -12,7 +12,10 @@ export class RequestValidationError extends Error {
 
     serializeErrors() {
         return this.errors.map(err => {
-            return {message: err.msg, field: err.type};
+            if (err.type == 'field') {
+             return {message: err.msg, field: err.path};
+            }
+            return {message: err.msg};
         });
     }
 }
