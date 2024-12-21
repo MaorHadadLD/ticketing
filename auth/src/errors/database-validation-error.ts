4f4171fe096dab66f99b45d4f6d7d72c593a73c6
MaorHadadLD <maorhadad94@gmail.com>
2024-12-21 15:52:13 +0200

    update errors validation

--- auth/src/errors/database-validation-error.ts
+++ auth/src/errors/database-validation-error.ts
@@ -1,9 +1,16 @@
 export class DatabaseConnectionError extends Error {
+    statusCode = 500;
     reason = 'Error connection to database';
 
     constructor() {
         super();
 
         Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
+    }
+
+    serializeErrors() {
+        return [
+            { message: this.reason }
+        ]
     }
 }
