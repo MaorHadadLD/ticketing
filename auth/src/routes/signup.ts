b65feff15622ea2c584e16aa54a53bc4a36b06a9
MaorHadadLD <maorhadad94@gmail.com>
2024-12-29 02:08:33 +0200

    formatting JSON properties + start Signin flow

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,8 +1,10 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
+
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
-import {BadRequestError} from '../errors/bad-request-error';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = express.Router();
 
@@ -29,13 +31,25 @@ router.post(
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      // console.log('Email in use');
-      // return res.send({});
       throw new BadRequestError('Email in use');
     }
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email
+      },
+      process.env.JWT_KEY!
+    );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt
+    };
 
     res.status(201).send(user);
   }
