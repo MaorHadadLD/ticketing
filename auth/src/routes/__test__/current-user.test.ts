a661efb5a06e99441f783e113850bbbc7fcf7926
MaorHadadLD <maorhadad94@gmail.com>
2025-01-06 20:00:22 +0200

    Adding an responds with details about the current user test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -1,19 +1,24 @@
-import request from 'supertest';
-import { app } from '../../app';
+import request from "supertest";
+import { app } from "../../app";
 
-it('responds with details about the current user', async () => {
-  await request(app)
-    .post('/api/users/signup')
+it("responds with details about the current user", async () => {
+  const authResponse = await request(app)
+    .post("/api/users/signup")
     .send({
-      email: 'test@test.com',
-      password: 'password'
+      email: "test@test.com",
+      password: "password",
     })
     .expect(201);
+  const cookie = authResponse.get("Set-Cookie");
 
+  if (!cookie) {
+    throw new Error("Cookie not set after signup");
+  }
   const response = await request(app)
-    .get('/api/users/currentuser')
+    .get("/api/users/currentuser")
+    .set("Cookie", cookie)
     .send()
     .expect(200);
 
-  console.log(response.body);
+  expect(response.body.currentUser.email).toEqual("test@test.com");
 });
