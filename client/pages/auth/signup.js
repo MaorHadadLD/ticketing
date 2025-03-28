de8655839c1d18a6f1c22da2b5103958786403af
MaorHadadLD <maorhadad94@gmail.com>
2025-03-28 22:43:47 +0300

    An onSuccess Callback

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,22 +1,24 @@
 import { useState } from 'react';
-import axios from 'axios';
+import Router from 'next/router';
 import useRequest from '../../hooks/use-request';
 
 export default () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const {doRequest, errors} = useRequste ({
+    const {doRequest, errors} = useRequest ({
         url: '/api/users/signup',
+        method: 'post',
         body: {
             email, 
             password
-        }
+        },
+        onSuccess: () => Router.push('/')
     });
 
     const onSubmit = async (event) => {
         event.preventDefault();
 
-        doRequest();
+        await doRequest();
     };
 
 
